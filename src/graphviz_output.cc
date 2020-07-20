5aa09954b7b7cc87f08431a93a5d2f3b28134e66
Felix Gündling <felix.guendling@gmail.com>
2020-07-20 08:48:40 +0200

    wip

--- src/graphviz_output.cc
+++ src/graphviz_output.cc
@@ -27,8 +27,8 @@ void graphiz_output(std::ostream& out, timetable const& tt) {
   }
   for (auto const& [name, t] : tt) {
     for (auto const& r : t->routes_) {
-      out << "    " << r->tag()
-          << R"( [ shape=box, fixedsize=false, style = "filled, bold", label=")"
+      out << "    " << r->tag() << R"([URL="#)" << r->tag()
+          << R"(", shape=box, fixedsize=false, style="filled, bold", label=")"
           << r->train_->name_ << ": "
           << (r->from_ == nullptr ? "START" : r->from_->name_) << " -> "
           << r->to_->name_ << "\\n"
