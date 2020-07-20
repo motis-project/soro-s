e866695326b4f3ead20e0fc928598eb8cddbe4c8
Felix Gündling <felix.guendling@gmail.com>
2020-07-20 19:30:35 +0200

    improve graphviz output

--- src/graphviz_output.cc
+++ src/graphviz_output.cc
@@ -12,6 +12,8 @@ namespace soro {
 void graphiz_output(std::ostream& out, timetable const& tt) {
   out << "digraph world {\n";
   out << R"--(
+    rankdir="LR";
+
     node [
       fixedsize=false,
       fontname=Monospace,
@@ -19,13 +21,20 @@ void graphiz_output(std::ostream& out, timetable const& tt) {
       height=2,
       width=2.5,
       style=bold,
-      fillcolor=white
+      color=white,
+      fillcolor=white,
+      shape=box
     ];
+
+    edge [
+      color=white
+    ];
+
 )--";
   for (auto const& [name, t] : tt) {
     out << "    subgraph cluster_" << t->name_ << " {\n";
-    out << "        style=filled;";
-    out << "        color=lightgrey;";
+    out << "        style=filled;\n";
+    out << "        color=lightgrey;\n";
     out << "        label=\"Train " << t->name_ << "\"\n";
     out << "        rank=\"same\"\n";
     for (auto const [i, r] : utl::enumerate(t->routes_)) {
@@ -37,9 +46,9 @@ void graphiz_output(std::ostream& out, timetable const& tt) {
   }
   for (auto const& [name, t] : tt) {
     for (auto const& r : t->routes_) {
-      out << "    " << r->tag() << R"([URL="#)" << r->tag()
-          << R"(", shape=box, fixedsize=false, style="filled, bold", label=")"
-          << r->train_->name_ << ": "
+      out << "    " << r->tag() << R"([URL="#)" << r->tag() << "\""
+          << (r->from_ == nullptr ? R"(, color=darkviolet, shape=ellipse)" : "")
+          << R"(, label=")" << r->train_->name_ << ": "
           << (r->from_ == nullptr ? "START" : r->from_->name_) << " -> "
           << r->to_->name_ << "\\n"
           << "sched@" << (r->from_ == nullptr ? "START" : r->from_->name_)
@@ -58,7 +67,14 @@ void graphiz_output(std::ostream& out, timetable const& tt) {
   for (auto const& [name, t] : tt) {
     for (auto const& r : t->routes_) {
       for (auto const& o : r->out_) {
-        out << "    " << r->tag() << " -> " << o->tag() << ";\n";
+        out << "    " << r->tag() << " -> " << o->tag();
+        if (r->from_ == nullptr) {
+          out << " [color=darkviolet, style=\"bold\"]";
+        } else if (r->train_ != o->train_) {
+          out << " [color=red, style=\"bold\", label=\"" << r->eotd_dpd_.first_
+              << "\"]";
+        }
+        out << ";\n";
       }
     }
   }
