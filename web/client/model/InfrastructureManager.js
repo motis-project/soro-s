9a84d2eeb930839208789c794bc9cc4a3ff41279
Julian Harbarth <harbarth@cs.tu-darmstadt.de>
2022-11-09 09:59:31 +0100

    dont use soro-client.js for now

--- web/client/model/InfrastructureManager.js
+++ web/client/model/InfrastructureManager.js
@@ -1,31 +1,7 @@
-import { getStationCoordPath } from "../utl/IDBFSHelper.js";
 import { getInfrastructureComponents } from "../utl/goldenLayoutHelper.js";
-import { Module } from "../soro-client.js";
 
 function loadInfrastructureFromIDBFS(filePath) {
-  /* Loading from cache is very slow atm, so disabled
-   const cachePath = new Module.FilesystemPath('/idbfs/cache/' + infrastructureFilename);
-   if (exists(cachePath.string())) {
-     this._current = new Module.Infrastructure(cachePath, true);
-   } else {
-   */
-
-  let infraOpts = new Module.InfrastructureOptions();
-  infraOpts.determine_conflicts = true;
-  infraOpts.infrastructure_path = new Module.FilesystemPath(filePath);
-  infraOpts.gps_coord_path = new Module.FilesystemPath(getStationCoordPath());
-
-  return new Module.Infrastructure(infraOpts);
-
-  /* try {
-     this._current.save(cachePath);
-     saveToPersistent();
-   } catch (e) {
-     console.error("Could not write serialized cache, maybe compiled without SERIALIZE?");
-     console.error(e);
-   }
-}
-   */
+  console.log("This is to be removed");
 }
 
 export class InfrastructureManager {
@@ -64,20 +40,12 @@ export class InfrastructureManager {
   }
 
   highlightSignalStationRoute(signalStationRouteID) {
-    if (!Module.validSSRID(signalStationRouteID)) {
-      return;
-    }
-
     for (const infrastructureComponent of getInfrastructureComponents()) {
       infrastructureComponent.highlightSignalStationRoute(signalStationRouteID);
     }
   }
 
   deHighlightSignalStationRoute(signalStationRouteID) {
-    if (!Module.validSSRID(signalStationRouteID)) {
-      return;
-    }
-
     for (const infrastructureComponent of getInfrastructureComponents()) {
       infrastructureComponent.deHighlightSignalStationRoute(signalStationRouteID);
     }
