6d02dc43773d960c99a78ee8bc4b816ddb30b97b
julianharbarth <julian@harbarth.io>
2022-11-19 17:12:54 +0100

    fix sassert for clangd

--- include/soro/utls/sassert.h
+++ include/soro/utls/sassert.h
@@ -1,49 +1,35 @@
 #pragma once
 
-#include <cassert>
 #include <chrono>
-#include <algorithm>
 #include <filesystem>
 #include <iomanip>
 #include <iostream>
-
-#if !defined(__EMSCRIPTEN__)
 #include <source_location>
-#endif
 
 // TODO(julian) replace this with the c++20 <format> header when available
 #include "fmt/ostream.h"
 
 namespace soro::utls {
 
-#if defined(__clang__)
-
 struct bool_with_loc {
-  constexpr bool_with_loc(bool const b) : b_(b) {}  // NOLINT
-
-  constexpr operator bool() const noexcept { return b_; }  // NOLINT
-
-  bool b_;
-};
-
+  bool_with_loc(bool const b, std::source_location const& loc =  // NOLINT
+#if !defined(__clang__)
+                              std::source_location::current())
 #else
+                              {})
+#endif
+      : b_(b), loc_(loc) {
+  }
 
-struct bool_with_loc {
-  constexpr bool_with_loc(bool const b, std::source_location const loc =
-                                            std::source_location::current())
-      : b_(b), loc_(loc) {}
-
-  constexpr operator bool() const noexcept { return b_; }  // NOLINT
+  operator bool() const noexcept { return b_; }  // NOLINT
 
   bool b_;
   std::source_location loc_;
 };
 
-#endif
-
 #if !defined(NDEBUG)
 template <typename Msg, typename... Args>
-inline void sassert(bool_with_loc const& assert_this, Msg&& msg, Args... args) {
+inline void sassert(bool_with_loc assert_this, Msg&& msg, Args... args) {
 
   if (!assert_this) {
     using clock = std::chrono::system_clock;
@@ -63,14 +49,15 @@ inline void sassert(bool_with_loc const& assert_this, Msg&& msg, Args... args) {
     fmt::print(ss, "\n");
 
 #if !defined(__clang__)
-    fmt::print(
-        ss, "[FAILED HERE] {}:{}:{} in {}",
-        std::filesystem::path(assert_this.loc_.file_name()).filename().string(),
-        assert_this.loc_.line(), assert_this.loc_.column(),
-        assert_this.loc_.function_name());
-    fmt::print(ss, "\n");
+    auto const& loc = assert_this.loc_;
+    fmt::print(ss, "[FAILED HERE] {}:{}:{} in {}",
+               std::filesystem::path(loc.file_name()).filename().string(),
+               loc.line(), loc.column(), loc.function_name());
+#else
+    fmt::print(ss, "[FAILED HERE] {}:{}:{} in {}", __builtin_FILE(),
+               __builtin_LINE(), __builtin_COLUMN(), __builtin_FUNCTION());
 #endif
-
+    fmt::print(ss, "\n");
     std::clog << ss.rdbuf();
 
     throw std::runtime_error(ss.str());
@@ -80,12 +67,22 @@ inline void sassert(bool_with_loc const& assert_this, Msg&& msg, Args... args) {
 #else
 
 template <typename Msg, typename... Args>
-inline void sassert(bool_with_loc const&, Msg&&, Args...) {}
+inline void sassert(bool const, Msg&&, Args...) {}
 
 #endif
 
-inline void sassert(bool_with_loc const& assert_this) {
+inline void sassert(bool const assert_this) {
   sassert(assert_this, "I didn't specify a error message :(");
 }
+
+#if !defined(NDEBUG)
+template <typename F>
+inline void sasserts(F&& f) {
+  f();
+}
+#else
+template <typename F>
+inline void sasserts(F&&) {}
+#endif
 
 }  // namespace soro::utls
