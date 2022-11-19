c98eec0dcc5262ded8b6cd08424464ffbbacc21a
julianharbarth <julian@harbarth.io>
2022-11-19 13:53:12 +0100

    don't use source_location with clang for now

--- include/soro/utls/sassert.h
+++ include/soro/utls/sassert.h
@@ -16,36 +16,15 @@
 
 namespace soro::utls {
 
-#if defined(__EMSCRIPTEN__)
+#if defined(__clang__)
 
-template <typename Msg, typename... Args>
-inline void sassert([[maybe_unused]] bool const assert_this,
-                    [[maybe_unused]] Msg&& msg, [[maybe_unused]] Args... args) {
+struct bool_with_loc {
+  constexpr bool_with_loc(bool const b) : b_(b) {}  // NOLINT
 
-#if !defined(NDEBUG)
-  if (!assert_this) {
-    using clock = std::chrono::system_clock;
+  constexpr operator bool() const noexcept { return b_; }  // NOLINT
 
-    auto const now = clock::to_time_t(clock::now());
-    struct tm tmp;
-#if _MSC_VER >= 1400
-    gmtime_s(&tmp, &now);
-#else
-    gmtime_r(&now, &tmp);
-#endif
-
-    fmt::print(std::clog, "[ASSERT FAIL][{}] ", std::put_time(&tmp, "%FT%TZ"));
-    fmt::print(std::clog, std::forward<Msg>(msg), std::forward<Args>(args)...);
-    fmt::print(std::clog, "\n");
-
-    std::abort();
-  }
-#endif
-}
-
-inline void sassert(bool const assert_this) {
-  sassert(assert_this, "I didn't specify a error message :(");
-}
+  bool b_;
+};
 
 #else
 
@@ -59,6 +38,8 @@ struct bool_with_loc {
   bool b_;
   std::source_location loc_;
 };
+
+#endif
 
 #if !defined(NDEBUG)
 template <typename Msg, typename... Args>
@@ -80,12 +61,15 @@ inline void sassert(bool_with_loc const& assert_this, Msg&& msg, Args... args) {
     fmt::print(ss, "[ASSERT FAIL][{}] ", std::put_time(&tmp, "%FT%TZ"));
     fmt::print(ss, std::forward<Msg>(msg), std::forward<Args>(args)...);
     fmt::print(ss, "\n");
+
+#if !defined(__clang__)
     fmt::print(
         ss, "[FAILED HERE] {}:{}:{} in {}",
         std::filesystem::path(assert_this.loc_.file_name()).filename().string(),
         assert_this.loc_.line(), assert_this.loc_.column(),
         assert_this.loc_.function_name());
     fmt::print(ss, "\n");
+#endif
 
     std::clog << ss.rdbuf();
 
@@ -103,7 +87,5 @@ inline void sassert(bool_with_loc const&, Msg&&, Args...) {}
 inline void sassert(bool_with_loc const& assert_this) {
   sassert(assert_this, "I didn't specify a error message :(");
 }
-
-#endif
 
 }  // namespace soro::utls
