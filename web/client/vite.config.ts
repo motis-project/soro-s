ee9ffbd9ea4005f615e8e8c8bf1da9ec7186be44
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2022-11-26 16:54:48 +0100

    Proxy infrastructure and timetable requests for now in vite

--- web/client/vite.config.ts
+++ web/client/vite.config.ts
@@ -1,4 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({ plugins: [vue()] })
+export default defineConfig({
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/infrastructure': {
+                target: 'http://0.0.0.0:8080',
+                changeOrigin: true,
+            },
+            '/timetable': {
+                target: 'http://0.0.0.0:8080',
+                changeOrigin: true,
+            },
+        },
+    },
+})
