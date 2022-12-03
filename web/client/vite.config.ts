08e44eee923994cd7985aa52b14ec3d6ebdd65c1
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2022-12-03 22:43:15 +0100

    Fix styling of vite config

--- web/client/vite.config.ts
+++ web/client/vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 const relayServer = 'http://0.0.0.0:8080';
 const relayLocations = [
-    '/infrastructure',
-    '/timetable',
-    '/font',
-    '/icons',
-    '/interlocking_route_simple',
-    '/small',
+	'/infrastructure',
+	'/timetable',
+	'/font',
+	'/icons',
+	'/interlocking_route_simple',
+	'/small',
 ];
 
 const proxy = {};
 relayLocations.forEach((location) => proxy[location] = {
-    target: relayServer,
-    changeOrigin: true,
+	target: relayServer,
+	changeOrigin: true,
 });
 export default defineConfig({
-    plugins: [vue()],
-    server: { proxy },
-})
+	plugins: [vue()],
+	server: { proxy },
+});
