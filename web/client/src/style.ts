2209ce8132c31b9b8724a5ce56f602202c67ba56
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-01-12 00:04:19 +0100

    Fix styles

--- web/client/src/style.ts
+++ web/client/src/style.ts
@@ -1,6 +1,6 @@
 import '../deps/GoldenLayout/css/goldenlayout-base.css';
 import '../deps/GoldenLayout/css/themes/goldenlayout-mdl-theme.css';
-import '../style/matter.css';
-import '../style/material-select.css';
-import '../style/material-icons.css';
-import '../style/style.css';
+import './style/matter.css';
+import './style/material-select.css';
+import './style/material-icons.css';
+import './style/style.css';
