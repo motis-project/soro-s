ba73df6e24f212fae55aa34e5e918101d54126c7
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-01-12 21:46:44 +0100

    register settings store globally

--- web/client/src/main.ts
+++ web/client/src/main.ts
@@ -4,11 +4,13 @@ import App from './App.vue';
 import { InfrastructureStore, InfrastructureNamespace } from '@/stores/infrastructure-store';
 import { TimetableStore, TimetableNamespace } from '@/stores/timetable-store';
 import { createStore } from 'vuex';
+import { SettingsNamespace, SettingsStore } from '@/stores/settings-store';
 
 const store = createStore({
     modules: {
         [InfrastructureNamespace]: InfrastructureStore,
         [TimetableNamespace]: TimetableStore,
+        [SettingsNamespace]: SettingsStore,
     }
 });
 const app = createApp(App);
