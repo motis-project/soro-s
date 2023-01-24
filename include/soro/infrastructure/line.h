df51c0aa2a7a74926729307a301c1a9b883d74da
julianharbarth <julian@harbarth.io>
2023-01-24 17:09:12 +0100

    split into .h/.cc

--- include/soro/infrastructure/line.h
+++ include/soro/infrastructure/line.h
@@ -16,40 +16,15 @@ struct line {
     bool signalling_{false};
   };
 
-  segment const& get_segment(kilometrage const km) const {
-    utls::sassert(
-        km >= segments_.front().from_,
-        "Kilometrage {} less than first kilometrage value {} of line {}.", km,
-        id_, segments_.front().from_);
-    utls::sassert(
-        km <= segments_.back().to_,
-        "Kilometrage {} more than last kilometrage value {} of line {}.", km,
-        id_, segments_.back().to_);
-    utls::sassert(!segments_.empty(), "Segments of line {} are empty.", id_);
-
-    auto const it =
-        std::lower_bound(
-            std::begin(segments_), std::end(segments_), km,
-            [](auto&& segment, auto&& v) { return segment.from_ < v; }) -
-        1;
-
-    utls::sassert(it->from_ <= km && km <= it->to_,
-                  "Segment of line {} does not contain kilometrage {}.", id_,
-                  km);
-
-    return *it;
-  }
-
-  bool has_signalling(kilometrage const km) const {
-    return get_segment(km).signalling_;
-  }
-
-  bool has_etcs(kilometrage const km) const { return get_segment(km).etcs_; }
+  bool has_signalling(kilometrage const km) const;
+  bool has_etcs(kilometrage const km) const;
 
   id id_;
 
   // ordered w.r.t. distance
   soro::vector<segment> segments_;
 };
+
+using lines = soro::map<line::id, line>;
 
 }  // namespace soro::infra
---
+++ src/infrastructure/line.cc
@@ -0,0 +1,43 @@
+#include "soro/infrastructure/line.h"
+
+#include "utl/logging.h"
+
+namespace soro::infra {
+
+line::segment const& get_segment(line const& l, kilometrage const km) {
+  utls::expects(
+      km >= l.segments_.front().from_,
+      "Kilometrage {} less than first kilometrage value {} of line {}.", km,
+      l.segments_.front().from_, l.id_);
+  utls::expects(!l.segments_.empty(), "Segments of line {} are empty.", l.id_);
+
+  // ideally this would be a precondition, alas
+  if (km > l.segments_.back().to_) {
+    uLOG(utl::warn) << "Requesting line segment with kilometrage " << km
+                    << ", but last line segment only goes to "
+                    << l.segments_.back().to_ << ". Serving last segment.";
+    return l.segments_.back();
+  }
+
+  auto const it =
+      std::lower_bound(
+          std::begin(l.segments_), std::end(l.segments_), km,
+          [](auto&& segment, auto&& v) { return segment.from_ < v; }) -
+      1;
+
+  utls::sassert(it->from_ <= km && km <= it->to_,
+                "Segment of line {} does not contain kilometrage {}.", l.id_,
+                km);
+
+  return *it;
+}
+
+bool line::has_signalling(kilometrage const km) const {
+  return get_segment(*this, km).signalling_;
+}
+
+bool line::has_etcs(kilometrage const km) const {
+  return get_segment(*this, km).etcs_;
+}
+
+}  // namespace soro::infra
