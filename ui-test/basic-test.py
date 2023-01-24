0616e3dc6676103bda83a6c6601c3afdd6913c11
Leon Vack <LogicalOverflow@users.noreply.github.com>
2023-01-24 14:30:41 +0100

    Story 8: Make UI Tests fail when a JavaScript error occurs (#43)
    * add a basic ui test with github actions intergraion
    
    * also run un-tests using firefox
    
    * make ui-tests fail on JS errors

---
+++ ui-test/basic-test.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import unittest
+import subprocess
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+target = "http://host.docker.internal:8080/"
+
+
+class SeleniumTestCase(unittest.TestCase):
+    """A base class for Selenium cases.
+
+    Handles common set up and tear down.
+    """
+
+    def setUp(self):
+        """Set up Selenium test resources.
+
+        Creates the Selenium driver connection and soro server process
+        """
+        opts = webdriver.ChromeOptions()
+        chromeDriver = webdriver.Remote(
+            command_executor="http://localhost:4444/wd/hub",
+            options=opts, keep_alive=True
+        )
+
+        opts = webdriver.FirefoxOptions()
+        firefoxDriver = webdriver.Remote(
+            command_executor="http://localhost:4444/wd/hub",
+            options=opts, keep_alive=True
+        )
+
+        self.drivers = [chromeDriver, firefoxDriver]
+
+        self.proc = subprocess.Popen([
+            "./soro-server", "--port", "8080",
+            "--resource_dir", "resources",
+            "--server_resource_dir", "server_resources"
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        for line in self.proc.stderr:
+            if b"tiles-server started on 0.0.0.0:" in line:
+                break
+
+    def tearDown(self):
+        """Tear down Selenium test resources.
+
+        Cleans up the Selenium driver connection and soro server process
+        """
+        for driver in self.drivers:
+            driver.quit()
+
+        self.proc.terminate()
+        self.proc.wait()
+        self.proc.stdout.close()
+        self.proc.stderr.close()
+
+    def assert_no_js_errors(self, driver):
+        """Assert the driver has not logged any JS errors."""
+        # firefox only supports WebDriver BiDi and log.entryAdded events from
+        # the W3C specs (https://github.com/mozilla/geckodriver/issues/284),
+        # which Selenium does not (yet) support
+        # (https://github.com/SeleniumHQ/selenium/issues/10335)
+        # So we just check chrome for logged JS errors
+        if driver.name == "chrome":
+            for line in driver.get_log("browser"):
+                if line['source'] != "javascript":
+                    continue
+                if line['level'] not in ["SEVERE"]:
+                    continue
+                self.fail("driver reported a JavaScript error " +
+                          f"({line['level']}): {line['message']}")
+
+
+class SimulationButtonTest(SeleniumTestCase):
+    """Test case validating the simulation button."""
+
+    def test_simulation_button(self):
+        """Basic tast of the simulation button.
+
+        Validates that clicking the button results in an active simulation tab.
+        """
+        for driver in self.drivers:
+            with self.subTest(f"driver {driver.name}"):
+                driver.get(target)
+
+                overlayToggle = driver.find_element(
+                    By.ID,
+                    "overlayToggleButton"
+                )
+                overlayToggle.click()
+
+                simButton = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (By.ID, "addInfrastructureComponentButton"))
+                )
+                simButton.click()
+                normClass = 'concat(" ", normalize-space(@class), " ")'
+                divFilter = " and ".join([
+                    f'contains({normClass}, " lm_active ")',
+                    f'contains({normClass}, " lm_tab ")',
+                    '@title="Simulation"'
+                ])
+                xpath = f'//section/div[{divFilter}]'
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, xpath))
+                )
+
+                self.assert_no_js_errors(driver)
+
+
+if __name__ == "__main__":
+    unittest.main()
